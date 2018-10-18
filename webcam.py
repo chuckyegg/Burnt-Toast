681b6726e069041a56f91b343a2972bf3479ed2c
chuckyegg <cgreen7@sheffield.ac.uk>
2018-10-18 07:55:13 +0100

    no message

--- webcam.py
+++ webcam.py
@@ -1,14 +1,12 @@
 import urllib.request
 
-def getImage(){
+def getImage():
 	
 	url = raw_input("What is the image URL?")
 	name = raw_input("Image name")
 	print('Downloading file from ' + url)
 
 	urllib.request.urlretrieve(url, '/home/pi/Programming/Burnt-Toast/temp/' + name)
-
-}
 
 getImage()
 '''
