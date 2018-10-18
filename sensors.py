62dc96867e386f77e3d0f79993ed8df4323d6bb9
chuckyegg <cgreen7@sheffield.ac.uk>
2018-10-18 08:52:24 +0100

    no message

--- sensors.py
+++ sensors.py
@@ -1,3 +1,21 @@
+def getMass():
+	mass = input("What is the mass? ")
+	print("Mass: " + mass)
+	return mass
+
+def getVolume()
+	volume = input("What is the volume? ")
+	print("Volume: " + volume)
+	return volume
+
+def getDensity()
+	mass = getMass();
+	volume = getVolume();
+	density = mass/volume
+	print("Density: " + density)
+	return Density
+
+getDensity()
 '''
 ###################################################################################
 This file manages all sensor readings and parsing
