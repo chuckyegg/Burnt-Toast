46b0e45598d7b5fd9facccc5d1e74c349351b313
chuckyegg <cgreen7@sheffield.ac.uk>
2018-10-18 08:05:27 +0100

    no message

--- visionApi.py
+++ visionApi.py
@@ -8,9 +8,23 @@ from google.cloud.vision import types
 
 client - vision.ImageAnnotatorClient()
 
-image = webcam.getImage();
+imageDir = webcam.getImage();
 
+fileName = os.path.join(
+	os.path.dirname(__file__),
+	imageDir)
 
+with io.open(fileName, 'rb') as imageFile:
+	content = imageFile.read()
+
+image = types.Image(content=content)
+
+response = client.label_detection(image=image)
+labels = response.lanel_annotations
+
+print('Labels:')
+for label in labels:
+	print(label.description)
 
 '''
 ###################################################################################
--- webcam.py
+++ webcam.py
@@ -2,8 +2,8 @@ import urllib.request
 
 def getImage():
 	
-	url = input("What is the image URL?")
-	name = input("Image name")
+	url = input("What is the image URL? ")
+	name = input("Image name: ")
 	print('Downloading file from ' + url)
 
 	urllib.request.urlretrieve(url, '/home/pi/Programming/Burnt-Toast/temp/' + name)
