27b14fdbfe2b887e28bc27871dd149f73127d83c
chuckyegg <cgreen7@sheffield.ac.uk>
2018-10-18 08:36:42 +0100

    no message

--- visionApi.py
+++ visionApi.py
@@ -1,39 +1,48 @@
+#Import modules to read from drive
 import io
 import os
 
+#Import custom webcam module to take picture with webcam
 import webcam
 
+#Import Google Cloud Vision Modules
 from google.cloud import vision
 from google.cloud.vision import types
 
+#Initalise Google Vision client
 client = vision.ImageAnnotatorClient()
 
-imageDir = webcam.getImage()
+#Define the get labels function
+def getLabels():
+	#Recieve image from webcam
+	imageDir = webcam.getImage()
 
-print(imageDir + ' Testing')
+	fileName = os.path.join(
+		os.path.dirname(__file__),
+		imageDir)
 
-fileName = os.path.join(
-	os.path.dirname(__file__),
-	imageDir)
+	with io.open(fileName, 'rb') as imageFile:
+		content = imageFile.read()
 
-with io.open(fileName, 'rb') as imageFile:
-	content = imageFile.read()
+	image = types.Image(content=content)
 
-image = types.Image(content=content)
+	labelResponse = client.label_detection(image=image)
+	propertyResponse = client.image_properties(image=image)
 
-response = client.label_detection(image=image)
-labels = response.label_annotations
+	properties = propertyResponse.image_properties_annotation
+	labels = response.label_annotations
+	#Return an array of labels from the vision API
+	print("Properties: " + properties)
+	print("Labels: " + labels)
 
-print('Labels:')
-for label in labels:
-	print(label.description)
+getLabels()
 
 '''
 ###################################################################################
 This file manages google vision api
 ###################################################################################
 vision
-	-get tags
+	-get labels
 	-get information
 	-get whatever
 ###################################################################################
