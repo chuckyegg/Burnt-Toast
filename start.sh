7676507768ee7ca679ae468579302db270c11797
chuckyegg <cgreen7@sheffield.ac.uk>
2018-10-18 07:30:47 +0100

    no message

--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Set virtualenv up
-source /home/pi/Programming/Burnt-Toast/env/etc/bin/activate
-#set JSON key
-export GOOGLE_APPLICATION_CREDENTIALS="/home/pi/Burnt Toast-b69c3a1fe6cc.json"
