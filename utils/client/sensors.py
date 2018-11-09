6e2439f4e685380b486142cfdc4980ca46a232a8
Charles Green <Eia16cg@shefuniad.shef.ac.uk>
2018-11-09 13:19:25 +0000

    sdfsdf

---
+++ utils/client/sensors.py
@@ -0,0 +1,38 @@
+def getMass():
+	mass = float(input("What is the mass? "))
+	print("Mass: " + str(mass))
+	return mass
+
+def getVolume():
+	volume = float(input("What is the volume? "))
+	print("Volume: " + str(volume))
+	return volume
+
+def getDensity():
+	mass = getMass();
+	volume = getVolume();
+	density = mass/volume
+	print("Density: " + str(density))
+	return density
+
+'''
+###################################################################################
+This file manages all sensor readings and parsing
+###################################################################################
+Mass sensor
+	-getMass()
+	-calibrate()
+	-getUncalibratedMass()
+	-
+###################################################################################
+Volume sensor
+	-getDistance()
+	-getVolume()
+	-calibrate()
+	-
+###################################################################################
+Viscosity sensor
+	-getViscosity()
+	-
+###################################################################################
+'''
