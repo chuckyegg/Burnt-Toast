f8630b29c157a7afa6bd37d86508f46195b7c767
chuckyegg <cgreen7@sheffield.ac.uk>
2019-02-15 15:16:31 +0000

    Updated server file

--- Main/server.js
+++ Main/server.js
@@ -1,11 +1,13 @@
 var express 	= require('express');
 var app 		= express();
 var fs 			= require("fs");
+cors = require('cors')(); 
+app.use(cors);
 var bodyParser 	= require('body-parser');
 var multer 		= require('multer');
 var mysql 		= require('mysql');
-var upload 		= multer();
-/*
+ var upload 		= multer();
+
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, './uploads')
@@ -16,14 +18,14 @@ var storage = multer.diskStorage({
 });
 
 var upload = multer({storage: storage});
-*/
 
-const vision = require('@google-cloud/vision');
-const client = new vision.ImageAnnotatorClient({
-		keyFilename: "/home/charles/credentials/Burnt-Toast.json"
-});
 
-var db 		= require('./utils/server/database.js');
+// const vision = require('@google-cloud/vision');
+// const client = new vision.ImageAnnotatorClient({
+// 		keyFilename: "/home/charles/credentials/Burnt-Toast.json"
+// });
+
+//var db 		= require('./utils/server/database.js');
 //var pic 	= require('./utils/server/visionapi.js')
 
 var con = mysql.createConnection({
@@ -33,20 +35,66 @@ var con = mysql.createConnection({
   database: "BURNTTOAST"
 });
 
-app.use(bodyParser.json()); 						
-app.use(bodyParser.urlencoded({ extended: true })); 	
 
-app.post('/imageLookup', upload.single("image"), async function (req, res) {
+app.use(bodyParser.json({limit:  '50mb'})); 						
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' })); 	
+
+// app.post('/imageLookup', upload.single("image"), async function (req, res) {
   
-    console.log(req.file);
-    console.log("image Lookup");
-    let result = await client.labelDetection(req.file.buffer);
-    labels = result[0].labelAnnotations
-    tags = []
-    labels.forEach(label => tags.push(label.description))
-    console.log(tags);
-    //db.addImage(con, 1, "test ingredient", "test tags", 1);
-    res.send(tags);
+//     console.log(req.file);
+//     console.log("image Lookup");
+//     let result = await client.labelDetection(req.file.buffer);
+//     labels = result[0].labelAnnotations
+//     tags = []
+//     labels.forEach(label => tags.push(label.description))
+//     console.log(tags);
+//     //db.addImage(con, 1, "test ingredient", "test tags", 1);
+//     res.send(tags);
+// })
+
+app.post('/imageSubmission', upload.single("image"), async function(req, res){
+  console.log("Image submission recieved")
+  name = req.body.name;
+  type = req.body.type;
+  mass = req.body.mass;
+  volume = req.body.mass;
+  density = req.body.density;
+  image = req.body.image;
+
+  con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected to MySQL");
+    var sql = "INSERT INTO ingredients (name, type, mass, volume, density, image) VALUES (?, ?, ?, ?, ?, ?)";
+    con.query(sql, [name, type, mass, volume, density, image], function (err, results){
+      if(err){
+        console.log(err);
+      }
+    })
+    console.log("1 record inserted");
+  })
+  res.sendStatus(200);
+})
+
+app.post('/experimentSubmission', async function(req, res){
+  console.log("Experiment Submission Recieved")
+  
+  var sqlIngredients = JSON.stringify({ingredients: req.body.ingredients});
+  var sqlResults = JSON.stringify({results: req.body.results});
+  console.log(sqlIngredients);
+  console.log(sqlResults)
+  con.connect(function(err){
+    if (err) throw err;
+    console.log("Connected to MySQL");
+    var sql = "INSERT INTO experiments (ingredients, results) VALUES (?, ?)";
+    con.query(sql, [sqlIngredients, sqlResults], function (err, results){
+      if (err){
+        console.log(err);
+      }
+      console.log("1 record inserted")
+    })
+  })
+  
+  res.sendStatus(200);
 })
 
 
